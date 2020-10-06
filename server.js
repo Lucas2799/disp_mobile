7aa2a486903d0c75d0065115015129756a09c358
Lucas Tavares <ltv2234@gmail.com>
2020-10-05 23:24:40 -0300

    atualização do front-end e correção de rota edit

--- server.js
+++ server.js
@@ -28,75 +28,70 @@ app.get('/', (req, res) => {
     res.render('lucas/index.ejs')
 })
 app.get('/', (req, res) => {
-    let cursor = db.collection('data').find()
+    let cursor = db.collection('livro').find()
 })
 
 app.get('/show', (req, res) => {
     
-    db.collection('data').find().toArray((err, results) => {
+    db.collection('livro').find().toArray((err, results) => {
         if (err) return console.log(err)
-        res.render('lucas/show.ejs', { data: results })
+        res.render('lucas/show.ejs', { livro: results })
 
     })
 })
 
 app.post('/show', (req, res) => {
-    db.collection('data').save(req.body, (err, result) => {
+    db.collection('livro').insertOne(req.body, (err, result) => {
         if (err) return console.log(err)
 
         console.log('Salvo no Banco de Dados')
         res.redirect('/show')
-        db.collection('data').find().toArray((err, results) => {
+        db.collection('livro').find().toArray((err, results) => {
             console.log(results)
         })
     })
     
 })
 //Rota para o roteamento de solicitação
-app.route('/edit/:id')
-.get((req, res) => {
+app.route('/lucas/edit/:id').get((req, res) => {
   var id = req.params.id
-  db.collection('data').find(ObjectId(id)).toArray(
-      (err, result) => {
+  db.collection('livro').find({"_id": ObjectId(id)}).toArray((err, result) => {
     if (err) return console.log(err)
-    res.render('lucas/edit.ejs', { data: result })
+    res.render('lucas/edit.ejs', {livro: result, title: 'Editar usuário'})
   })
 })
-.post((req, res) => {
-    var id = req.params.id
+app.route('/lucas/update').post((req, res) => {
+    var id = req.body.id
+    console.log(id);
     var nome = req.body.nome
     var fantasia = req.body.fantasia
     var cnpj = req.body.cnpj
     var cidade = req.body.cidade
     var tipo = req.body.tipo
    
-    db.collection('data').updateOne(
-        {
-            _id: ObjectId(id)
-        }, 
-        {
-            $set: {
-              name: nome,
-              name: fantasia,
-              name: cnpj,
-              name: cidade,
-              name: tipo
-      }
-    }, (err, result) => {
-      if (err) return console.log(err)
-      res.redirect('/show')
-      console.log('Atualizado no Banco de Dados')
+    db.collection('livro').updateOne(
+        {_id: ObjectId(id)},
+
+        {$set: {
+          nome: nome, 
+          fantasia: fantasia,
+          cnpj: cnpj, 
+          cidade : cidade,
+           tipo: tipo} 
+          }, 
+        (err, result) => {
+        if (err) return res.send(err)
+        res.redirect('/show')
+        console.log('Atualizado no Banco de Dados')
     })
   })
-  app.route('/delete/:id')
+  app.route('/lucas/delete/:id')
   .get((req, res) => {
   var id = req.params.id
  
-  db.collection('data').deleteOne({_id: ObjectId(id)}, (err, result) => {
+  db.collection('livro').deleteOne({_id: ObjectId(id)}, (err, result) => {
     if (err) return res.send(500, err)
     console.log('Deletado do Banco de Dados!')
     res.redirect('/show')
   })
 })
-
-
